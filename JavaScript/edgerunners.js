4b35a2ced25de18acc3d8f9135917c37ac89e423
aski94 <alesaubr@gmail.com>
2024-05-12 00:53:20 +0200

    1.1

---
+++ JavaScript/edgerunners.js
@@ -0,0 +1,67 @@
+const menuButton = document.getElementById("hamburgerMenu");
+const menu = document.getElementById("openMenu");
+const galleryContainer = document.getElementById("galleryContainer");
+const images = document.querySelectorAll('.galleryImage');
+const leftButton = document.getElementById("leftArrowHolder");
+const rightButton = document.getElementById("rightArrowHolder");
+const hiddenCards = document.querySelectorAll('.hidden');
+
+const observerCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("show");
+            observer.unobserve(entry.target);
+        }
+        else {
+            entry.target.classList.remove("show");
+        }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback);
+
+hiddenCards.forEach((card) => observer.observe(card));
+
+// Re-observe elements when user scrolls above them
+window.addEventListener('scroll', () => {
+    hiddenCards.forEach((card) => {
+        const rect = card.getBoundingClientRect();
+        if (rect.bottom > 0) { // Change the condition here
+            observer.observe(card);
+        }
+    });
+});
+
+
+
+//Zdroj: https://chat.openai.com/share/30b7b9b5-54de-4805-8061-10fc83452e30
+leftButton.addEventListener("click", function () {
+    const lastImage = galleryContainer.lastElementChild;
+    galleryContainer.insertBefore(lastImage, galleryContainer.firstElementChild);
+});
+
+rightButton.addEventListener("click", function () {
+    const firstImage = galleryContainer.firstElementChild;
+    galleryContainer.appendChild(firstImage);
+});
+
+menuButton.addEventListener("click", slide);
+window.addEventListener("resize", checkWidth);
+
+function slide() {
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+
+    menuButton.classList.toggle("open");
+}
+//Zdroj: https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth
+function checkWidth() {
+    if (window.innerWidth > 1350) {
+        menu.style.display = "none";
+        menuButton.classList.remove("open")
+    }
+}
+
